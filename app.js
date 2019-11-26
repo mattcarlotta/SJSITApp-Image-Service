50e39a72cdcbe5d7d1776858d5f69e3fc77c29c4
Matt <carlotta.matt@gmail.com>
2019-11-26 14:24:57 -0800

    Add morgan timestamp

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import moment from "moment-timezone";
 
 const app = express();
 
+moment.tz.setDefault("America/Los_Angeles");
 //= ===========================================================//
 // EXPRESS SERVE IMAGES
 //= ===========================================================//
+morgan.token("date", () => moment().format("MMMM Do YYYY, h:mm:ss a"));
+
 app.use(cors());
-app.use(morgan("tiny"));
+app.use(
+  morgan(
+    ":remote-addr [:date] :referrer :method :url HTTP/:http-version :status :res[content-length]",
+  ),
+);
 app.use("/", express.static("images"));
 app.listen(4000, () => {
   console.log(
-    `\nImage microservice is up and running on port \x1b[1m4000\x1b[0m`,
+    "\nImage microservice is up and running on port \x1b[1m4000\x1b[0m",
   );
 });
